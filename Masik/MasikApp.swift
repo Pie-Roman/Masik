dfb21e40383bea8dd97a74e36e4f4946224a0ba5
Pie-Roman <py.romann@yandex.ru>
2025-06-08 18:35:43 +0300

    Initial Commit

---
+++ Masik/MasikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasikApp.swift
+//  Masik
+//
+//  Created by Роман Ломтев on 08.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MasikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
